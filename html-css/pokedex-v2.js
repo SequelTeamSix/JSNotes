ce144dd4ed13bcce23e7432f4320522fa3a2a898
tedbeast <tedbalashov@gmail.com>
2022-03-16 11:32:44 -0400

    pokedex v2

---
+++ html-css/pokedex-v2.js
@@ -0,0 +1,108 @@
+/*
+    What do we need?
+
+    Our toolbox:
+
+    HTML
+        -Bootstrap later
+    CSS
+        -selectors
+    JS
+        -Fetch API
+            -Promises, async/await
+            -JSON
+        -Event listeners
+        -DOM manipulation
+
+    dataInput : our input box
+    getData : our button
+    data: our HTML section
+*/
+
+var dataInputBox = document.getElementById('dataInput');
+var getDataButton = document.getElementById('getData');
+var dataSection = document.getElementById('data');;
+const apiURL = 'http://pokeapi.co/api/v2/pokemon/'
+
+/*
+Monday we took care of events by saying something like button.onclick = doSomething()
+Instead we pass in the method type, as well as a function that we will use as a *callback*
+Meaning: a callback function is a function passed in as a parameter that will be executed later
+*/
+
+getDataButton.addEventListener('click', generateData);
+
+/*async function:
+meaning, asynchronous function:
+it allows for asynchronous execution while waiting for an await*/
+async function generateData() {
+    /*
+        rudimentary DOM manipulation:
+        DOM?
+        Document Object Model
+        it's a tree representation of the structure of the page
+    */
+    /*dataSection.innerHTML = 'PokePlaceholder';*/
+    let userInput = dataInputBox.value;
+    /*
+    *Await*
+    (wait)
+    for a
+    *Promise*
+    (some variable that will eventually be filled)
+    */
+    let response = await fetch (apiURL + userInput);
+    console.log(response);
+    if(response.status === 200){
+        let data = await response.json();
+        console.log(data);
+        populateData(data);
+    }else{
+        dataSection.innerHTML = 'It Got Away!';
+    }
+}
+function populateData(pokemonObject) {
+    /*
+    Clear the datasection so that we don't have multiple bulbasaurs
+    */
+    dataSection.innerHTML='';
+    /*
+    Create a nametag
+    */
+    let nameTag = document.createElement('h3');
+    nameTag.innerHTML = pokemonObject.name;
+    dataSection.appendChild(nameTag);
+    for(let spriteNum in pokemonObject.sprites){
+        /*
+            Because JS doesn't ever care much about types,
+            every variable is technically equivalent to a boolean
+            what this means is certain variables are false and certain are true
+            generally this follows a sensible pattern:
+            null, 0, and '' are false
+            most everything else is true
+            These are called 'truthy' and 'falsy' values
+
+            This stems from a larger understanding that JS is *loosely types*
+            as opposed to Java, *strongly typed*
+        */
+        if(pokemonObject.sprites[spriteNum] && (typeof pokemonObject.sprites[spriteNum] != 'object')) {
+            let spriteImg = document.createElement('img');
+            spriteImg.src = pokemonObject.sprites[spriteNum];
+            dataSection.appendChild(spriteImg);
+        }
+        /*
+        ul: unordered list
+        ol: ordered list
+        both contain 'li' (list items) as children
+        */
+        
+    }
+    let abilitiesList = document.createElement('ul');
+        for(let abilityNum in pokemonObject.abilities){
+            let ability = document.createElement('li');
+            ability.innerHTML = pokemonObject.abilities[abilityNum].ability.name;
+            abilitiesList.appendChild(ability);
+        }
+        dataSection.appendChild(abilitiesList);
+
+}
--- html-css/pokedex.js
+++ html-css/pokedex.js
@@ -1,53 +1,63 @@
-var apiURL = 'http://pokeapi.co/api/v2/pokemon/';
+let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+
 document.getElementById('GetData').onclick = getData;
-function getData() {
-    // AJAX
-    var userInput = document.getElementById('PokedexIDInput').value;
-    // 4 steps to ajax:
-    // step 1: create a xmlhttprequestobject
 
-    var xhttp = new XMLHttpRequest();
+async function getData() {
+    // If using input for identifiers, etc.
+    // For example, if using PokeAPI, this may be the Pokemon's ID.
+    let userInput = document.getElementById('PokedexIDInput').value; 
 
-    // set a callback function for when the ready state changes
-    xhttp.onreadystatechange = receiveData;
+    // Emptying out the div before inserting new data.
+    document.getElementById('data').innerHTML = '';
 
-    // open a request
-    xhttp.open('GET', apiURL + ''+ userInput);
+    let response = await fetch(apiURL + userInput);
 
-    // send the request
-    xhttp.send();
-
-    function receiveData(){
-        /*
-        AJAX has several different states:
-        0:unsent
-        1:opened
-        2:headers received
-        3:loading
-        4:done
-        */
-       var dataSection = document.getElementById('data');
-       dataSection.innerHTML = '';
-       if(xhttp.readyState === 4){
-           if(xhttp.status === 200){
-            //    200 as http response means 'OK'
-            var response = xhttp.responseText;
-            response = JSON.parse(response);
-            populateData(response, dataSection);
-            console.log(response);
-           }
-       }
+    if (response.status === 200) {
+        let data = await response.json();
+        populateData(data);
+    } else {
+        dataSection.innerHTML = 'It Got Away!';
     }
 }
-function populateData(response, section){
-    var nameTag = document.createElement('h3');
-    nameTag.innerHTML=response.name;
 
-    section.appendChild(nameTag);
+function populateData(response) {
+    console.log(response);
+    let dataSection = document.getElementById('data');
+    
+    let nameTag = document.createElement('h3');
+    nameTag.innerHTML =  capitalize(response.name);
+    let abilitiesArray = response.abilities;
+    let abilities = document.createElement('ul');
+    for (const ability of abilitiesArray)
+    {
+        let abilityLi = document.createElement('li');
+        abilityLi.innerHTML = capitalize(ability.ability.name);
+        abilities.appendChild(abilityLi);
+    }
 
-    var sprites = response.sprites;
+    dataSection.appendChild(nameTag);
+    dataSection.innerHTML += 'Abilities<br>';
+    dataSection.appendChild(abilities);
 
-    var spriteImg = document.createElement('img');
-    spriteImg.src = sprites.back_default;
-    section.appendChild(spriteImg);
+    let spritesObject = response.sprites;
+    for (const sprite in spritesObject) {
+        if(spritesObject[sprite]) {
+            if((typeof spritesObject[sprite]) != 'object'){
+                let spriteImg = document.createElement('img');
+                spriteImg.src = spritesObject[sprite];
+                dataSection.appendChild(spriteImg);
+            }
+        }
+    }
+}
+
+/*
+    The PokeAPI returns Pokemon's information as all lowercase.
+    This function is used to fix this when processing data.
+*/
+function capitalize(str) {
+    if (str)
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    else
+        return '';
 }
